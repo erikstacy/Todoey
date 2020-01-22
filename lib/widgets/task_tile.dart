309bab78ffbc9569d0ffb5e64a203761a548c6ab
erikstacy <erikslaststand@gmail.com>
2020-01-22 14:19:47 -0600

    Created state for the Add Task screen

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,15 +1,61 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+// * 
+// *
+// * Task Tile declaration
+// *
+// *
+class TaskTile extends StatefulWidget {
+
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task'),
-      trailing: Checkbox(
-        onChanged: (value) {},
-        value: false,
+      title: Text(
+        'This is a task',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
       ),
+      trailing: TaskCheckbox(
+        checkboxState: isChecked,
+        toggleCheckboxState: (bool checkboxState) {
+          setState(() {
+            isChecked = checkboxState;
+          });
+        },
+      ),
+    );
+  }
+}
+
+// * 
+// *
+// * Task Checkbox declaration
+// *
+// *
+class TaskCheckbox extends StatelessWidget {
+
+  TaskCheckbox({
+    this.checkboxState,
+    this.toggleCheckboxState,
+  );
+
+  final bool checkboxState;
+  final Function toggleCheckboxState;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: toggleCheckboxState,
     );
   }
 }
